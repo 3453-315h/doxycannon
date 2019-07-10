07f34880c2fbbd7e8b8e675538abef05dc641278
routeback <frankcass1@gmail.com>
2019-07-10 18:21:39 -0400

    Re-write

--- reset.sh
+++ reset.sh
@@ -1,15 +1,52 @@
 #!/bin/bash
-# reset.sh
-# Swap out *.ovpn files if you think an IP is burned. Replaces *.ovpn found in the VPN folder with N number of new configs. N is equal to the maximum number of simultaneous connections your VPN provider allows.
-# Make sure you have all of your *.ovpn files in a directory named 'configs'
+#
+# Name: reset.sh
+# Auth: Frank Cass
+# Date: 20190710
+# Desc: Quickly change Proton OpenVPN configuration profiles and reset the docker containers.
+# 	Useful when performing an external assessment and your IP is blacklisted.
+#	The number of profiles you specify should not exceed the maximum number of connections allowed by your VPN provider.
+#	Put all of your OVPN files in configs/ and the script will move the active ones to VPN/
+#
+#	Troubleshooting: May have to run inside a virutalenv.
+#	Try:
+#		pip install virtualenv
+#		virtualenv env
+#		source env/bin/activate
+#
+###
 
-read -p "[+] Enter the number of profiles you want to use: " n
-echo "[+] Bringing down the cannon..."
-./doxycannon.py --down
-echo "[+] Removing current VPN profiles."
-rm -r VPN/*.ovpn 2>&1 >/dev/null
-for i in $(ls configs/ | sort -R | tail -n $n); do echo "[+] Copying random profile: $i"; cp configs/$i VPN/$i; done
-echo "[+] Profiles replaced."
-./doxycannon.py --build
-echo "[+] Ready, Aim, Fire!"
-exit 0
+clear
+
+echo "[*] $0: Doxycannon Profile Switcher"
+
+# Check that configs exists and contains OVPN files
+profiles=(`find configs/ -maxdepth 1 -name "*.ovpn"`)
+if [ ${#profiles[@]} -gt 0 ]
+then
+        : # Do nothing if the file(s) actually exist.
+else
+         echo "[!] EXIT: No OpenVPN (.ovpn) files found in configs."
+         exit 1
+fi
+
+# Check if Docker is running
+if [ "$(systemctl is-active docker)" = "active" ]; then
+	echo "[*] Docker appears to be running."
+else
+	echo "[!] EXIT: Docker is not running!"
+	echo "[*] Try: service docker start"
+	exit 1
+fi
+
+# Request user input
+read -p "[*] Enter the number of profiles you want to use: " n
+read -p "[*] Enter Country Code (e.g. US): " c
+
+# Arm the cannon
+echo "[*] Bringing down the cannon..."; ./doxycannon.py --down
+echo "[*] Removing current VPN profiles."; rm -r VPN/*.ovpn > /dev/null 2>&1
+echo "[*] Copying new profiles."; for i in $(ls configs/ | sort -R | grep "$c" | tail -n $n); do echo "[*] Copying random profile: $i"; cp configs/$i VPN/$i; done; echo "[*] Profiles replaced."
+echo "[*] Building new configuration..."; ./doxycannon.py --build
+echo "[*] Ready, Aim, Fire!"; ./doxycannon.py --up
+./doxycannon.py --single
