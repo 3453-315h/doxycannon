6b43048cd82a12345a7362d6c55b908c38b3fb4f
routeback <frankcass1@gmail.com>
2019-07-11 14:26:06 -0400

    Force python3 usage, check if VPN/auth.txt exists, replace resolv scripts in *.OVPN files, specify auth.txt instead of prompting for user/pass

--- reset.sh
+++ reset.sh
@@ -18,13 +18,26 @@
 
 clear
 
-echo "[*] $0: Doxycannon Profile Switcher"
+echo "[*] Doxycannon Profile Switcher"
 
 # Check that configs exists and contains OVPN files
 profiles=(`find configs/ -maxdepth 1 -name "*.ovpn"`)
 if [ ${#profiles[@]} -gt 0 ]
 then
-        : # Do nothing if the file(s) actually exist.
+	# Modify OVPN configs per https://github.com/audibleblink/doxycannon/issues/14
+	echo "[*] OpenVPN Configuration Files Found."
+	# Perform check for auth.txt in VPN folder
+		if [ ! -f "VPN/auth.txt" ]; then
+			echo "[!] EXIT: No auth.txt file found for VPN authentication!"
+			echo "[*] Create auth.txt in the VPN folder with two lines, containing your username and password."
+			exit 1
+		else
+			: # Do nothing
+		fi
+	sed -i 's/up \/etc\/openvpn\/update-resolv-conf/up \/etc\/openvpn\/up.sh/g' configs/*.ovpn
+        sed -i 's/down \/etc\/openvpn\/update-resolv-conf/down \/etc\/openvpn\/down.sh/g' configs/*.ovpn
+	sed -i '/.*auth-user-pass*/c\auth-user-pass auth.txt' configs/*.ovpn
+        : # Do nothing else if the file(s) actually exist and have been successfully modified.
 else
          echo "[!] EXIT: No OpenVPN (.ovpn) files found in configs."
          exit 1
@@ -39,14 +52,22 @@ else
 	exit 1
 fi
 
+python=$(which python3)
+if [ $python = "" ]; then
+	echo "[!] EXIT: Python3 installation not found! Set the path to the executable directly in this script."
+	exit 1
+else
+	:
+fi
+
 # Request user input
 read -p "[*] Enter the number of profiles you want to use: " n
 read -p "[*] Enter Country Code (e.g. US): " c
 
 # Arm the cannon
-echo "[*] Bringing down the cannon..."; ./doxycannon.py --down
+echo "[*] Bringing down the cannon..."; $python ./doxycannon.py --down
 echo "[*] Removing current VPN profiles."; rm -r VPN/*.ovpn > /dev/null 2>&1
 echo "[*] Copying new profiles."; for i in $(ls configs/ | sort -R | grep "$c" | tail -n $n); do echo "[*] Copying random profile: $i"; cp configs/$i VPN/$i; done; echo "[*] Profiles replaced."
-echo "[*] Building new configuration..."; ./doxycannon.py --build
-echo "[*] Ready, Aim, Fire!"; ./doxycannon.py --up
-./doxycannon.py --single
+echo "[*] Building new configuration..."; $python ./doxycannon.py --build
+echo "[*] Ready, Aim, Fire!"; $python ./doxycannon.py --up
+$python ./doxycannon.py --single
